858905276b4d433732e526d9900d5c80cf2ab936
Diana Verevkina <diana_verevkina@mail.ru>
2023-04-27 23:17:30 +0300

    sidebar.js added

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,17 +18,52 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    const bodyElement = document.querySelector('body');
+    const sidebarToggle = bodyElement.querySelector('.sidebar-toggle');
 
+    function openSidebar() {
+      if (bodyElement.classList.contains('sidebar-open')) {
+        bodyElement.classList.remove('sidebar-open', 'sidebar-collapse');
+        return;
+      }
+
+      bodyElement.classList.add('sidebar-open', 'sidebar-collapse');
+    }
+
+    sidebarToggle.addEventListener('click', openSidebar);
   }
 
   /**
    * При нажатии на кнопку входа, показывает окно входа
    * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
+   * При нажатии на кнопку регистрации показывает окно регистрации
    * При нажатии на кнопку выхода вызывает User.logout и по успешному
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
+    const sidebarMenu = document.querySelector('.sidebar-menu');
+    const menuItems = sidebarMenu.querySelectorAll('.menu-item');
 
+    function openAuthModal() {
+      if (this.classList.contains('menu-item_login')) {
+        App.getModal('login').open();
+      }  
+      
+      if (this.classList.contains('menu-item_register')) {
+        App.getModal('register').open();
+      }
+      
+      if (this.classList.contains('menu-item_logout')) {
+        User.logout(response => {
+          if (response.success) {
+            App.setState('init');
+          }
+        })
+      }
+    }
+    
+    menuItems.forEach(menuItem => {
+      menuItem.addEventListener('click', openAuthModal);
+    })
   }
 }
